82d52a7ece3baa353efb05e648c4c44d06b440cc
Рафаиль <k00priz@mail.ru>
2023-09-08 23:37:00 +0700

    Добавил сборку в cmake файл для qtcreator и подключил виджеты теперь проект client собирается с графическим окном

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,19 +1,24 @@
-
 #include "include/tcp_client.h"
 #include <iostream>
 #include <thread>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 
 using namespace Serf;
 
 int main(int argc, char* argv[]) {
 
+    QApplication app(argc, argv);
+
+   // QLabel label("Hello, world!");
+   // label.show();
 
     tcpClient client {"localhost", 1234};
 
-    jsonparser pars; 
-    std::string key = "formula";    
+    jsonparser pars;
+    std::string key = "formula";
 
-    client.OnMessage = [&](const std::string& message) { 
+    client.OnMessage = [&](const std::string& message) {
         std::cout  << message;
     };
     std::thread t{[&client] () { client.Run(); }};
@@ -28,7 +33,9 @@ int main(int argc, char* argv[]) {
 
     client.Stop();
     t.join();
-    return 0;
+
+
+    return app.exec();
 }
 
 
@@ -44,36 +51,36 @@ int main() {
     std::cin >> inText;
 
     boost::asio::io_service io_service;
-  
-    // Создание сокета 
+
+    // Создание сокета
     tcp::socket socket(io_service);
-  
-    // Соединение 
+
+    // Соединение
     socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ));
-  
-    // Запрос/сообщение от клиента 
-    const string msg = parser.TranslateTextJson(inText) + "\n"; 
+
+    // Запрос/сообщение от клиента
+    const string msg = parser.TranslateTextJson(inText) + "\n";
 
     boost::system::error_code error;
     boost::asio::write( socket, boost::asio::buffer(msg), error );
      if( !error ) {
         cout << "Клиент отправил приветственное сообщение!" << endl;
-     }  
+     }
      else {
         cout << "отправить не удалось: " << error.message() << endl;
-     }  
-  
-    // Получение ответа от сервера  
+     }
+
+    // Получение ответа от сервера
     boost::asio::streambuf receive_buffer;
     boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
      if( error && error != boost::asio::error::eof ) {
         cout << "получить не удалось: " << error.message() << endl;
-     }  
+     }
      else {
         const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
         cout << data << endl;
-     }  
-     
+     }
+
      return 0;
-} 
+}
 */
