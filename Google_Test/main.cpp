6035f0ac32587f07523ff08e74a66e2045e34427
Рафаиль-Мухутдинов <k00priz@mail.ru>
2024-02-02 14:26:31 +0700

    Added Google tests to the server

---
+++ Google_Test/main.cpp
@@ -0,0 +1,40 @@
+
+#include "../Server/include/jsonparser.h"
+#include "gtest/gtest.h"
+
+// создаем тестовый класс, унаследованный от ::testing::Test
+class jsonparserTest : public ::testing::Test {
+protected:
+    // определяем метод SetUp(), который будет вызываться перед каждым тестом
+    void SetUp() override {
+        // создаем объект класса jsonparser
+        jp = new Serf::jsonparser();
+    }
+
+    // определяем метод TearDown(), который будет вызываться после каждого теста
+    void TearDown() override {
+        // удаляем объект класса jsonparser
+        delete jp;
+    }
+
+    // объявляем указатель на объект класса jsonparser
+    Serf::jsonparser* jp;
+};
+
+// создаем тестовый метод, который будет проверять корректность работы метода StringSorting()
+TEST_F(jsonparserTest, StringSortingTest) {
+    // задаем тестовую строку
+    std::string message = "127.0.0.1:37316: {\"formula\":\"5+5/2\"}\n";
+    // вызываем метод StringSorting() с тестовой строкой
+    jp->StringSorting(message);
+   
+    // проверяем, что id и json имеют ожидаемые значения
+    ASSERT_EQ(jp->GetId(), "127.0.0.1:37316");
+    ASSERT_EQ(jp->GetJson(), "{\"formula\":\"5+5/2\"}\n");
+}
+
+// запускаем все тесты
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
